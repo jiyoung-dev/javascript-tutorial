8ea9b4e1bb07a5e8ab04c8bd60c623bc082f60ed
Jiyoung <janice10508@gmail.com>
2021-08-14 16:57:24 +0900

    #4.2 Events

--- momentum/app.js
+++ momentum/app.js
@@ -1,26 +1,12 @@
 const loginInput = document.querySelector("#login-form input");
-const loginButton = document.querySelector("#login-form button");
+const loginForm = document.querySelector('#login-form');
 
-function onLoginBtnClick() {
-  // console.dir(loginInput);
-  // console.log("hello", loginInput.value);
-
-  /* input 이름 유효성검사 
-    - username이 없으면 안됨
-    - username에 길이 제한을 둠
-  */
+// form submit은 기본적으로 브라우저 새로고침 후에 동작한다.  
+function onLoginSubmit(event) {
+  event.preventDefault(); // 브라우저의 기본동작을 막아주는 함수 
   const username = loginInput.value;
-  // if(username === ""){
-  //   alert("Please write your name");
-  // } else if(username.length >= 15) {
-  //   alert("Your name is too long.)
-  // }
-  // else{
-  //   console.log("hello", loginInput.value);
-
-  // html form 태그로 유효성검사 
   console.log(username);
-  }
+}
 
-
-loginButton.addEventListener("click", onLoginBtnClick);
+// submit은 Enter를 누르거나, 버튼을 클릭할때 발생하는 이벤트
+loginForm.addEventListener("submit", onLoginSubmit);
