3b37ae3df3d02a6c440b097f58e372101488dc5c
Jiyoung <janice10508@gmail.com>
2021-08-14 18:04:14 +0900

    #4.6 Loading Username

--- momentum/app.js
+++ momentum/app.js
@@ -1,17 +1,32 @@
 const loginInput = document.querySelector("#login-form input");
 const loginForm = document.querySelector('#login-form');
-const greeting = document.querySelector(".hidden");
+const greeting = document.querySelector("#greeting");
+
 const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username";
 
 // form submit은 기본적으로 브라우저 새로고침 후에 동작한다.  
 function onLoginSubmit(event) {
   event.preventDefault(); // 브라우저의 기본동작을 막아주는 함수 
   loginForm.classList.add(HIDDEN_CLASSNAME);
   const username = loginInput.value;
-  localStorage.setItem("username", username);
+  localStorage.setItem(USERNAME_KEY, username);
+  paintGreetings(username);
+}
+
+function paintGreetings(username){
   greeting.innerText = `Hello ${username}`;
   greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
-// submit은 Enter를 누르거나, 버튼을 클릭할때 발생하는 이벤트
-loginForm.addEventListener("submit", onLoginSubmit);
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+
+if(savedUsername === null){  //local Storage에 유저정보가 없다면, form의 submit을 기다렸다가 input으로부터 유저정보를 받고, 
+                            //input에서 받은 user를 가진 paintGreetings를 호출할것이다. 
+  // show the form 
+  loginForm.addEventListener("submit", onLoginSubmit);
+  loginForm.classList.remove(HIDDEN_CLASSNAME);
+} else {  // local Storage에 유저정보가 있는 경우, 유저정보를 인자로 넣어준다 
+  // show the greetings
+  paintGreetings(savedUsername);
+}
