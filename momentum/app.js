9047da9059f911ced892c69149c8fa001a26e862
Jiyoung <janice10508@gmail.com>
2021-08-14 17:26:48 +0900

    #4.4 Getting Username

--- momentum/app.js
+++ momentum/app.js
@@ -1,11 +1,15 @@
 const loginInput = document.querySelector("#login-form input");
 const loginForm = document.querySelector('#login-form');
+const greeting = document.querySelector(".hidden");
+const HIDDEN_CLASSNAME = "hidden";
 
 // form submit은 기본적으로 브라우저 새로고침 후에 동작한다.  
 function onLoginSubmit(event) {
   event.preventDefault(); // 브라우저의 기본동작을 막아주는 함수 
+  loginForm.classList.add(HIDDEN_CLASSNAME);
   const username = loginInput.value;
-  console.log(username);
+  greeting.innerText = `Hello ${username}`;
+  greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
 // submit은 Enter를 누르거나, 버튼을 클릭할때 발생하는 이벤트
