6e7018f7e19ef68d8997b63aa75b1702c720da12
Jiyoung <janice10508@gmail.com>
2021-08-16 12:40:44 +0900

    #7.5 Loading To Dos part Two
    1. 로컬 저장소에 todo 저장
    2. data 추가시, 로컬에 update (새로고침해도 안사라짐)

--- momentum/js/todo.js
+++ momentum/js/todo.js
@@ -2,6 +2,15 @@ const toDoForm = document.getElementById("todo-form");
 const toDoList = document.getElementById("todo-list");
 const toDoInput = toDoForm.querySelector("#todo-form input");
 
+const TODOS_KEY = "todos";
+
+let toDos = [];
+// local storage에 저장 
+function saveToDos(){
+  localStorage.setItem(TODOS_KEY, JSON.stringify(toDos));  // 문자열 변환함수
+  
+}
+
 // todo 삭제함수 
 function deleteToDo(event){
   const deleteTarget = event.target.parentElement;
@@ -29,8 +38,21 @@ function handleToDoSubmit(event){
   // console.log(newTodo);
   // Enter 누르면 placeholder 비우기 
   toDoInput.value = "";
+  toDos.push(newTodo); // toDos 배열에 newTodo 전달 
   paintToDo(newTodo);
+  saveToDos();
 }
 
 toDoForm.addEventListener("submit", handleToDoSubmit);
 
+const savedToDos = localStorage.getItem(TODOS_KEY);
+
+// TODO: toDos 배열에 들어오는 기존값 -> 새로고침 후에도 계속 유지, 새로운 data 추가시, update 기능 
+
+if (saveToDos){
+  const parsedToDos = JSON.parse(savedToDos);
+  toDos = parsedToDos;
+  // forEach: array의 각각의 item에 대해 함수 실행 (주로, 화살표와 익명함수를 함께 사용)
+  parsedToDos.forEach(paintToDo);
+
+}
