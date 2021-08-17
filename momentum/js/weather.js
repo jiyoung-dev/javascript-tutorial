133d7f1d2e7e6786a2e0cb2a2625ca3bdd856c89
Jiyoung <janice10508@gmail.com>
2021-08-17 22:20:29 +0900

    # Using weather API
    1. 날씨 API: https://openweathermap.org/
    2. response.json() 메서드 사용: 도시, 기상정보 파싱

--- momentum/js/weather.js
+++ momentum/js/weather.js
@@ -1,8 +1,20 @@
+const city = document.querySelector("#weather span:first-child");
+const weather = document.querySelector("#weather span:last-child");
+const API_KEY = "f0f6390c45c893b352d575905fc41b22";
+
 // user 위치정보 확인 
 function onGeoOk(position){
   const lat = position.coords.latitude;
-  const lng = position.coords.longitude;
-  console.log(lat, lng);
+  const lon = position.coords.longitude;
+
+  // Using open weather map API
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      city.innerText = data.name;
+      weather.innerText = `${data.weather[0].main}, ${data.main.temp}`;
+    });
 }
 
 function onGeoError(){
