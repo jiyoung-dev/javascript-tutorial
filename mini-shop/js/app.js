b89d6a05f33fca51cf26520cfdfc54ccb69b4f66
Jiyoung <janice10508@gmail.com>
2021-08-27 15:46:57 +0900

    Add Button event
    카테고리별 버튼 클릭시, 해당 리스트만 반환하기

--- mini-shop/js/app.js
+++ mini-shop/js/app.js
@@ -1,5 +1,4 @@
-// items 리스트 모두 불러오기 (막힘)
-// json을 저렇게 작성하는게 맞는지?
+// items 리스트 모두 불러오기
 function loadItems(){
   return fetch('data/data.json')
     // 받아온 데이터가 성공적이라면 json으로 반환 
@@ -24,11 +23,34 @@ function createHTML(item){
   `;
 }
 
+// Button 이벤트 
+function onButtonClick(event, items){
+  // HTML 요소로부터 data 어트리뷰트 값 불러오기  
+  const key = event.target.dataset.key;
+  const value = event.target.dataset.value;
+
+  // .btn 영역 클릭시 null인경우 undefined로 값이 저장되는 경우, 무시 
+  if(key == null || value == null){
+    return; 
+  }
+
+  displayItems(items.filter(item => item[key] === value)) ;
+
+}
+
+// filtering 이벤트 함수 
+function setEventListeners(items){
+  const logo = document.querySelector('.logo');
+  const button = document.querySelector('.btn');
+  logo.addEventListener('click', () => displayItems(items));
+  button.addEventListener('click', event => onButtonClick(event, items));
+}
+
 // main
 loadItems()
   .then(items => {
     console.log(items);
     displayItems(items);
-    // setEventListeners(items)
+    setEventListeners(items)
   })
   .catch(console.log('error'));
